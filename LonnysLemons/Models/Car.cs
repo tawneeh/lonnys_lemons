3ad87fb45209f18d9a0a309eb70629865f8155f3
Tawnee Harris <tawneeh@icloud.com>
2020-12-15 14:19:38 -0800

    add MaxPrice
    Co-authored-by: Kaila Sprague McRae <kaila.sprague@icloud.com>

--- LonnysLemons/Models/Car.cs
+++ LonnysLemons/Models/Car.cs
@@ -1,45 +1,41 @@
-namespace Dealership.Models
+using System.Collections.Generic;
+
+namespace LonnysLemons.Models
 {
   public class Car
   {
-    private string _makeModel;
-    private int _price;
-    private int _miles;
-    public static string MakeSound(string sound)
-    {
-      return "Our cars sound like " + sound;
-    }
+    public string MakeModel { get; set; }
+    public int Price { get; set; }
+    public int Miles { get; set; }
+    // public int MaxPrice { get; set; }
+    public static List<Car> CarsMatchingSearch = new List<Car>(0);
+
+    private static List<Car> _instances = new List<Car> {};
 
     public Car(string makeModel, int price, int miles)
     {
-      _makeModel = makeModel;
-      _price = price;
-      _miles = miles;
+      MakeModel = makeModel;
+      Price = price;
+      Miles = miles;
+      _instances.Add(this);
     }
 
-    public string GetMakeModel()
+    public static List<Car> GetAll()
     {
-      return _makeModel;
+      return _instances;
+    }
+    public static List<Car> WorthBuying(int maxPrice)
+    {
+      // MaxPrice = maxPrice;
+      foreach (Car car in Cars)
+      {
+        if (Price <= maxPrice)
+        {
+          CarsMatchingSearch.Add(this);
+        }
+        return CarsMatchingSearch;
+      }
     }
 
-    public int GetPrice()
-    {
-      return _price;
-    }
-
-    public int GetMiles()
-    {
-      return _miles;
-    }
-
-    public bool WorthBuying(int maxPrice)
-    {
-      return (_price <= maxPrice);
-    }
-
-    public void SetPrice(int newPrice)
-    {
-      _price = newPrice;
-    }
   }
 }
